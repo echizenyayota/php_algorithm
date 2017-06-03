b129fe480cab131d7c8542364f52cacacbdedfa9
echizenyayota <virtualmisso30@gmail.com>
2017-06-03 18:22:32 +0900

    P50 追加と削除

--- chapter1/array-ins-del.php
+++ chapter1/array-ins-del.php
@@ -8,3 +8,21 @@ $a[] = 4;
 array_push($a, 5);
 array_unshift($a, 0);
 print_r($a);
+
+// 削除
+// 配列の先頭の値を削除
+$v1 = array_shift($a);
+echo $v1 . "\n";
+// 配列の最後の値を削除
+$v2 = array_pop($a);
+echo $v2 . "\n";
+print_r($a);
+
+// --- 削除 ---
+// 配列の先頭の値を削除
+// $v1 = array_shift($a);
+// echo $v1 . "\n";
+// 配列の最後の値を削除
+// $v2 = array_pop($a);
+// echo $v2 . "\n";
+// print_r($a);
