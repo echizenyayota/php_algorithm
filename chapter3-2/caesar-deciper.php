c82173ee091b537285c81be4a9bebe9cc1f9d372
echizenyayota <virtualmisso30@gmail.com>
2017-07-24 08:29:37 +0900

    substr関数の確認

--- chapter3-2/caesar-deciper.php
+++ chapter3-2/caesar-deciper.php
@@ -6,13 +6,17 @@ function makeTable($shift) {
   $alp_s = "abcdefghijklmnopqrstuvwxyz";
   $shift = $shift % strlen($alp_b);
   $alp_b_shift = substr($alp_b, $shift).substr($alp_b, 0, $shift);
+  $alp_b_shift_2 = substr($alp_b, $shift);
+  $alp_b_shift_3 = substr($alp_b, 0, $shift);
   // $alp_s_shift = substr($alp_b, $shift).substr($alp_b, 0, $shift);
-  $table = [];
-  $data = gettype($alp_b_shift);
-  echo $data;
+  // $table = [];
+  var_dump($alp_b_shift_2);
+  var_dump($alp_b_shift_3);
+  // $data = gettype($alp_b_shift);
+  // echo $data;
 }
 
-makeTable(1);
+makeTable(2);
 
 // // 暗号文
 // $s = trim(file_get_contents("encrypt.txt"));
