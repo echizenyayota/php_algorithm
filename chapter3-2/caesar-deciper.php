bb139b5b1cc39fffcfa96123f93adde99c39a224
echizenyayota <virtualmisso30@gmail.com>
2017-07-29 17:09:51 +0900

    P118シーザー暗号を解読しよう

--- chapter3-2/caesar-deciper.php
+++ chapter3-2/caesar-deciper.php
@@ -5,27 +5,51 @@ function makeTable($shift) {
   $alp_b = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   $alp_s = "abcdefghijklmnopqrstuvwxyz";
   $shift = $shift % strlen($alp_b);
+  // var_dump($shift);
+  // exit;
   $alp_b_shift = substr($alp_b, $shift).substr($alp_b, 0, $shift);
-  $alp_b_shift_2 = substr($alp_b, $shift);
-  $alp_b_shift_3 = substr($alp_b, 0, $shift);
-  // $alp_s_shift = substr($alp_b, $shift).substr($alp_b, 0, $shift);
-  // $table = [];
-  var_dump($alp_b_shift_2);
-  var_dump($alp_b_shift_3);
-  // $data = gettype($alp_b_shift);
-  // echo $data;
+  // var_dump($alp_b_shift);
+  // exit;
+  $alp_s_shift = substr($alp_s, $shift).substr($alp_s, 0, $shift);
+  // var_dump($alp_s_shift);
+  // exit;
+  $table = [];
+  for ($i = 0; $i < strlen($alp_b);$i++) {
+    $c1 = substr($alp_b, $i, 1);
+    // var_dump($alp_b);
+    // exit;
+    $c2 = substr($alp_b_shift, $i, 1);
+    // var_dump($alp_s);
+    // exit;
+    $table[$c2] = $c1;
+    // var_dump($table);
+    // var_dump($table[$c2]);
+    $c1 = substr($alp_s, $i, 1);
+    // var_dump($c1);
+    $c2 = substr($alp_s_shift, $i, 1);
+    // var_dump($c2);
+    $table[$c2] = $c1;
+    // var_dump($table);
+    // var_dump($table[$c2]);
+  }
+  return $table;
 }
 
-makeTable(2);
+function decode($str, $shift) {
+  $table = makeTable($shift);
+  $res = "";
+  for ($i = 0; $i < strlen($str); $i++) {
+    $c = substr($str, $i, 1);
+    $res .= isset($table[$c]) ? $table[$c] : $c;
+  }
+  return $res;
+}
 
-// // 暗号文
-// $s = trim(file_get_contents("encrypt.txt"));
-// //一文字ずつずらして解読に挑戦
-// for ($i = 1; $i <= 25; $i++) {
-//   $dec = decode($s, $i);
-//   echo "[$i] $dec\n";
-//   var_dump($dec);
-// }
-
-
- ?>
+// 暗号文
+$s = trim(file_get_contents("encrypt.txt"));
+//一文字ずつずらして解読に挑戦
+for ($i = 1; $i <= 25; $i++) {
+  $dec = decode($s, $i);
+  echo "[$i] $dec\n";
+  var_dump($dec);
+}
